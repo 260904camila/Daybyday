ebc515ac6a425c53a345783ba191f40686d51f99
260904camila <cs007296@gmail.com>
2024-04-18 09:29:55 -0300

    o

--- CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.daybyday.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.daybyday.model.Servico;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    
-}
